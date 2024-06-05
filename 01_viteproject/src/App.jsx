3ca9104774abaf4918dd2a6b8e7dc666dccad71c
Aanchal Patidar <aanchalpatidar89@gmail.com>
2024-06-05 14:33:15 +0530

    My first basic program on react

---
+++ 01_viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+   <h1>Hii, this is my first react project.</h1>
+  )
+}
+
+export default App
---
+++ 02_viteproject/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Hello , I am here to learn react.</h1>
+  )
+}
+
+export default App
