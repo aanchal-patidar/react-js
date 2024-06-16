c466f667cdde5ec4661aea95d017c5700e200b54
Aanchal Patidar <aanchalpatidar89@gmail.com>
2024-06-16 19:32:57 +0530

    appwrites and components

--- 13_megaBlog/src/components/index.js
+++ 13_megaBlog/src/components/index.js
@@ -1,6 +1,10 @@
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import Container from './Container/Container'
+import Logo from './logo'
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
 
 export {
-    Header,Footer
+    Header,Footer,Container,Logo,LogoutBtn,RTE
 }
